812b725a6f63f1eba4ef0522ed18125cf648481d
goren11-droid <goren11@gmail.com>
2026-05-12 14:57:50 +0300

    Create sb.js

---
+++ netlify/functions/sb.js
@@ -0,0 +1,42 @@
+const SB_BASE = 'https://test.smartbee.co.il/api/v1';
+
+exports.handler = async (event) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+    'Content-Type': 'application/json'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 204, headers, body: '' };
+  }
+
+  try {
+    const path = event.queryStringParameters?.path || '';
+    const method = event.httpMethod;
+    const authHeader = event.headers['authorization'] || event.headers['Authorization'] || '';
+
+    const fetchHeaders = { 'Content-Type': 'application/json' };
+    if (authHeader) fetchHeaders['Authorization'] = authHeader;
+
+    const response = await fetch(SB_BASE + path, {
+      method,
+      headers: fetchHeaders,
+      body: method !== 'GET' ? event.body : undefined
+    });
+
+    const data = await response.json();
+    return {
+      statusCode: response.status,
+      headers,
+      body: JSON.stringify(data)
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
